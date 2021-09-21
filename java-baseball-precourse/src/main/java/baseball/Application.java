739d4dd93436d586f1177d8e5812b15f66e8cbec
jihunparkme <jihunpark.tech@gmail.com>
2021-09-21 10:53:37 +0900

    feat(baseball): create skeleton code

--- java-baseball-precourse/src/main/java/baseball/Application.java
+++ java-baseball-precourse/src/main/java/baseball/Application.java
@@ -1,17 +1,13 @@
-package baseball; // 패키지명을 통해 어떤 프로그램인지 한 눈에 알아볼 수 있도록 !!!
+package baseball;
 
-import baseball.domain.Calculator;
-
+/**
+ * 객체 지향 프로그래밍
+ * 1. 기능을 가지고 있는 클래스를 인스턴스화(=객체)
+ * 2. 필요한 기능을 (역할에 맞는) 각 인스턴스가 수행 (의인화)
+ * 3. 각 결과를 종합
+ */
 public class Application {
     public static void main(String[] args) {
-        Calculator teacher = new Calculator(); // Calculator class 의 인스턴스
-        System.out.println(teacher.add(1, 3));
-        System.out.println(teacher.result);
 
-        Calculator calculator = new Calculator();
-        System.out.println(calculator.add(2000, 21));
-        System.out.println(calculator.result);
-
-        System.out.println(teacher.shareResult);
     }
 }
