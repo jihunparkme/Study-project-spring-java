97fbd3af7dc5ab7c0440e2aedf680dfa9bf2fdf2
jihunparkme <jihunpark.tech@gmail.com>
2021-09-21 12:25:20 +0900

    test(baseball): remove duplicates

--- java-baseball-precourse/src/test/java/baseball/domain/RefereeTest.java
+++ java-baseball-precourse/src/test/java/baseball/domain/RefereeTest.java
@@ -3,6 +3,8 @@ package baseball.domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,6 +18,19 @@ class RefereeTest {
         referee = new Referee();
     }
 
+    /**
+     * 묶음 테스트
+     */
+    @ParameterizedTest
+    @CsvSource({"1,2,3,0 볼 3 스트라이크", "7,8,9,아웃", "2,3,1,3 볼 0 스트라이크", "1,3,2,2 볼 1 스트라이크"})
+    public void compare(int number1, int number2, int number3, String expected) {
+        String result = referee.compare(ANSWER, Arrays.asList(number1, number2, number3));
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+
+    /**
+     * 개별 테스트
+     */
     @Test
     void 스트라이크3() {
         String result = referee.compare(ANSWER, Arrays.asList(1, 2, 3));
