e36ccf9dfbf0b2c727642c44d2af936933a58b49
jihunparkme <jihunpark.tech@gmail.com>
2021-09-21 11:55:05 +0900

    feat(baseball): Implement the function complete

--- java-baseball-precourse/src/main/java/baseball/Application.java
+++ java-baseball-precourse/src/main/java/baseball/Application.java
@@ -4,8 +4,10 @@ import baseball.domain.Judgment;
 import baseball.domain.NumberGenerator;
 import baseball.domain.Referee;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * 객체 지향 프로그래밍
@@ -15,6 +17,25 @@ import java.util.List;
  */
 public class Application {
     public static void main(String[] args) {
+
+//        test();
+
+        NumberGenerator generator = new NumberGenerator();
+        List<Integer> computer = generator.createRandomNumbers();
+
+        Referee referee = new Referee();
+
+        String result = "";
+        while (!result.equals("0 볼 3 스트라이크")) {
+            result = referee.compare(computer, askNumbers());
+            System.out.println("result = " + result);
+        }
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    private static void test() {
+
         NumberGenerator generator = new NumberGenerator();
         List<Integer> randomNumbers = generator.createRandomNumbers();
         System.out.println("randomNumbers = " + randomNumbers);
@@ -29,5 +50,18 @@ public class Application {
         Referee referee = new Referee();
         String compare = referee.compare(Arrays.asList(7,8,9), Arrays.asList(1, 2, 3));
         System.out.println("compare = " + compare);
+    }
+
+    private static List<Integer> askNumbers() {
+        System.out.print("숫자를 입력해 주세요 : ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        List<Integer> numbers = new ArrayList<>();
+        for (String number: input.split("")) {
+            numbers.add(Integer.valueOf(number));
+        }
+
+        return numbers;
     }
 }
