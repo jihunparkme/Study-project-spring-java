1bd8e962edb8179d1acbf3a3b4d10577a7a71ad9
jihunparkme <jihunpark.tech@gmail.com>
2021-09-21 11:16:17 +0900

    feat(baseball): Implement the function of how many numbers are the same

--- java-baseball-precourse/src/main/java/baseball/domain/Judgment.java
+++ java-baseball-precourse/src/main/java/baseball/domain/Judgment.java
@@ -4,10 +4,16 @@ import java.util.List;
 
 public class Judgment {
     public int correctCount(List<Integer> computer, List<Integer> player) {
+        int result = 0;
+        for (int i = 0; i < player.size(); i++) {
+            if (player.contains(computer.get(i))) {
+                result++;
+            }
+        }
         return 0;
     }
 
-    public boolean hasPlace(int placeNumber, int number) {
+    public boolean hasPlace(List<Integer> computer, int placeNumber, int number) {
         return false;
     }
 }
