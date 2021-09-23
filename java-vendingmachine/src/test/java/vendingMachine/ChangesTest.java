173059982a44b9920e5aaaae68db3b8ae03f0d32
jihunparkme <jihunpark.tech@gmail.com>
2021-09-23 20:15:22 +0900

    TDD(vendingmachine) : n원이_남아_있다면_n원_동전_1개를_돌려_준다 중복 제거

--- java-vendingmachine/src/test/java/vendingMachine/ChangesTest.java
+++ java-vendingmachine/src/test/java/vendingMachine/ChangesTest.java
@@ -1,9 +1,7 @@
 package vendingMachine;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -17,33 +15,5 @@ class ChangesTest {
         Changes changes = new Changes(value);
         List<CoinSet> coinSet = changes.coin();
         assertThat(coinSet).containsExactlyInAnyOrder(coin);
-    }
-
-    @Test
-    void _10원이_남아_있다면_10원_동전_1개를_돌려_준다() {
-        Changes changes = new Changes(10);
-        List<CoinSet> coinSet = changes.coin();
-        assertThat(coinSet).containsExactlyInAnyOrder(CoinSet._10_COIN);
-    }
-
-    @Test
-    void _50원이_남아_있다면_50원_동전_1개를_돌려_준다() {
-        Changes changes = new Changes(50);
-        List<CoinSet> coinSet = changes.coin();
-        assertThat(coinSet).containsExactlyInAnyOrder(CoinSet._50_COIN);
-    }
-
-    @Test
-    void _100원이_남아_있다면_100원_동전_1개를_돌려_준다() {
-        Changes changes = new Changes(100);
-        List<CoinSet> coinSet = changes.coin();
-        assertThat(coinSet).containsExactlyInAnyOrder(CoinSet._100_COIN);
-    }
-
-    @Test
-    void _500원이_남아_있다면_500원_동전_1개를_돌려_준다() {
-        Changes changes = new Changes(500);
-        List<CoinSet> coinSet = changes.coin();
-        assertThat(coinSet).containsExactlyInAnyOrder(CoinSet._500_COIN);
     }
 }
