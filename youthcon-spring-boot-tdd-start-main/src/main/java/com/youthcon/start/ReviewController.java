a57ef8e3b880f08b5f89a6bac94207873a6c50be
jihun.park <jihunpark.tech@gmail.com>
2022-01-07 20:42:52 +0900

    [youthcon spring boot tdd] Controller 임시 코드 작성 후 테스트 성공 만들기

---
+++ youthcon-spring-boot-tdd-start-main/src/main/java/com/youthcon/start/ReviewController.java
@@ -0,0 +1,14 @@
+package com.youthcon.start;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReviewController {
+
+    @GetMapping("/reviews/{id}")
+    public Object getById(@PathVariable Long id) {
+        return null;
+    }
+}
