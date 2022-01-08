e2ac7c03bb4052048a03067b80f5948ae7eed959
jihun.park <jihunpark.tech@gmail.com>
2022-01-08 22:04:11 +0900

    [youthcon spring event] @TransactionalEventListener 를 활용해 트랜잭션 단위를 제어

--- youthcon-spring-event/src/main/java/com/youthcon21/handsonlab/springevent/admin/application/CouponEventListener.java
+++ youthcon-spring-event/src/main/java/com/youthcon21/handsonlab/springevent/admin/application/CouponEventListener.java
@@ -2,14 +2,15 @@ package com.youthcon21.handsonlab.springevent.admin.application;
 
 import com.youthcon21.handsonlab.springevent.user.event.UserCouponEvent;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
 public class CouponEventListener {
 
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void onApplicationEvent(UserCouponEvent event) {
         log.info("쿠폰 등록 완료 : {}", event.getEmail());
     }
