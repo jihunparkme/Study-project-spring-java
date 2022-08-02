64093987e6ec9fe50a49620533d888fab80abef1
Jihun Park <jihunpark.tech@gmail.com>
2022-08-02 22:28:17 +0900

    add: figure interface

---
+++ java-coordinate-playground-practice/src/main/java/coordinate/Figure.java
@@ -0,0 +1,13 @@
+package coordinate;
+
+import java.util.List;
+
+public interface Figure {
+    List<Point> getPoints();
+
+    boolean hasPoint(int x, int y);
+
+    double area();
+
+    String getAreaInfo();
+}
