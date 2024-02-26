398a9fb06745cdcb458abf32ba72e7fb6d52b69c
Jihun Park <jihunpark.tech@gmail.com>
2024-02-26 14:07:04 +0900

    [Order API TDD] 상품 조회 기능 API 테스트로 전환

--- product-order-service/src/test/java/com/example/productorderservice/product/ProductApiTest.java
+++ product-order-service/src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -1,5 +1,8 @@
 package com.example.productorderservice.product;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
@@ -16,4 +19,20 @@ class ProductApiTest extends ApiTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
+    @Test
+    void 상품조회() {
+        ProductSteps.requestAddProduct(ProductSteps.getAddProductRequest());
+        Long productId = 1L;
+
+        final ExtractableResponse<Response> response = RestAssured.given().log().all() // 요청 로그 남기기
+                .when()
+                .get("/products/{productId}", productId)
+                .then().log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getString("id")).isEqualTo("1");
+        assertThat(response.jsonPath().getString("name")).isEqualTo("상품명");
+        assertThat(response.jsonPath().getString("price")).isEqualTo("1000");
+    }
 }
