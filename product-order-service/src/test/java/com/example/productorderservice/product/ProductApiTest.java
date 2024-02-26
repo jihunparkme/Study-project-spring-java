45c3b6e33810b7495f46addcc8f8a2177119a54d
Jihun Park <jihunpark.tech@gmail.com>
2024-02-26 13:35:09 +0900

    [Order API TDD] rest-assured 테스트 격리 코드 추가

--- product-order-service/src/test/java/com/example/productorderservice/product/ProductApiTest.java
+++ product-order-service/src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -4,17 +4,12 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ProductApiTest extends ApiTest {
-
-    @Autowired
-    private ProductService productService;
-
 
     @Test
     void 상품등록() {
