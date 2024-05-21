b62362078e6aaec1157f77f214bbb39bb560145b
Jihun Park <jihunpark.tech@gmail.com>
2024-05-21 21:55:30 +0900

    [stock concurrency issues] fix: failed test

--- stock-concurrency/src/test/java/com/example/stock/service/StockServiceTest.java
+++ stock-concurrency/src/test/java/com/example/stock/service/StockServiceTest.java
@@ -2,7 +2,6 @@ package com.example.stock.service;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -36,7 +35,7 @@ class StockServiceTest {
 
     @Test
     void decrease_inventory() {
-        stockService.decreases(1L, 1L);
+        stockService.decrease(1L, 1L);
 
         final Stock stock = stockRepository.findById(1L).orElseThrow();
 
@@ -46,23 +45,24 @@ class StockServiceTest {
     @Test
     void decrease_inventory_concurrent_requests() throws InterruptedException {
         int threadCount = 100;
-        final ExecutorService executorService = Executors.newFixedThreadPool(32);
-        final CountDownLatch latch = new CountDownLatch(threadCount); // 다른 스레드에서 수행 중인 작업이 완료될 때까지 대기할 수 있도록 지원
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);  // 다른 스레드에서 수행 중인 작업이 완료될 때까지 대기할 수 있도록 지원
 
         for (int i = 0; i < threadCount; i++) {
             executorService.submit(() -> {
                 try {
-                    stockService.decreases(1L, 1L);
+                    stockService.decrease(1L, 1L);
                 } finally {
                     latch.countDown();
                 }
             });
-
-            latch.await();
         }
 
-        final Stock stock = stockRepository.findById(1L).orElseThrow();
+        latch.await();
 
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 - (100 * 1) = 0
         assertEquals(0, stock.getQuantity());
     }
 }
