db1e69c46ab0cf3c50a3f015aecc7c0ffb1db653
Jihun Park <jihunpark.tech@gmail.com>
2024-05-21 22:17:29 +0900

    [stock concurrency issues] Optimistic Lock

--- stock-concurrency/src/main/java/com/example/stock/domain/Stock.java
+++ stock-concurrency/src/main/java/com/example/stock/domain/Stock.java
@@ -1,9 +1,6 @@
 package com.example.stock.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -18,6 +15,9 @@ public class Stock {
     private Long productId;
 
     private Long quantity;
+
+    @Version
+    private Long version;
 
     public Stock(final Long productId, final Long quantity) {
         this.productId = productId;
---
+++ stock-concurrency/src/main/java/com/example/stock/facade/OptimisticLockStockFacade.java
@@ -0,0 +1,25 @@
+package com.example.stock.facade;
+
+import com.example.stock.service.OptimisticLockStockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class OptimisticLockStockFacade {
+
+    private final OptimisticLockStockService optimisticLockStockService;
+
+    public void decrease(Long id, Long quantity) throws InterruptedException {
+        // 업데이트 실패 시 재시도
+        while (true) {
+            try {
+                optimisticLockStockService.decrease(id, quantity);
+                break;
+            } catch (Exception e) {
+                Thread.sleep(50);
+            }
+        }
+    }
+}
