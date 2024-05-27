f03ddea95877fd5998a5da6ddf72333b33f85655
Jihun Park <jihunpark.tech@gmail.com>
2024-05-27 21:50:05 +0900

    [First Come, First Served Event] refactor: HashMap -> Map

--- first-come-first-served-coupon-system/api/src/main/java/org/example/api/config/KafkaProducerConfig.java
+++ first-come-first-served-coupon-system/api/src/main/java/org/example/api/config/KafkaProducerConfig.java
@@ -10,13 +10,14 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
 
     @Bean
     public ProducerFactory<String, Long> producerFactory() {
-        final HashMap<String, Object> config = new HashMap<>();
+        final Map<String, Object> config = new HashMap<>();
 
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
