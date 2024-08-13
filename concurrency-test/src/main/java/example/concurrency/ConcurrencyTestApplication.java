d05de63cdceba6e34410402e41af345956b8d3a0
Jihun Park <jihunpark.tech@gmail.com>
2024-08-13 19:30:30 +0900

    [concurrency test] init: project

---
+++ concurrency-test/src/main/java/example/concurrency/ConcurrencyTestApplication.java
@@ -0,0 +1,13 @@
+package example.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyTestApplication.class, args);
+    }
+
+}
