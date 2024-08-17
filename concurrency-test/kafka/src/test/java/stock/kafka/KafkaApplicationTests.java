8d76984cd87c71e774dce05e1bb9d0c9db201697
Jihun Park <jihunpark.tech@gmail.com>
2024-08-17 13:05:54 +0900

    [concurrency test] feat: init kafka consume project

---
+++ concurrency-test/kafka/src/test/java/stock/kafka/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package stock.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
