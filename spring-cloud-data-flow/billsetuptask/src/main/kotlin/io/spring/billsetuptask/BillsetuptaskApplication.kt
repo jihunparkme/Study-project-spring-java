dae3b00f3f545b53767b1eb894a2ae715fa82960
Jihun Park <jihunpark.tech@gmail.com>
2024-10-11 22:37:57 +0900

    [SPDF] init project

---
+++ spring-cloud-data-flow/billsetuptask/src/main/kotlin/io/spring/billsetuptask/BillsetuptaskApplication.kt
@@ -0,0 +1,11 @@
+package io.spring.billsetuptask
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BillsetuptaskApplication
+
+fun main(args: Array<String>) {
+	runApplication<BillsetuptaskApplication>(*args)
+}
