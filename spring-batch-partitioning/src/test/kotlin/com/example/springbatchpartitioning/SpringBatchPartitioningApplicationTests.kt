0411953b5dfa6306de56dfb1655c794bbb8622d5
Jihun Park <jihunpark.tech@gmail.com>
2026-01-19 21:27:29 +0900

    init: project

---
+++ spring-batch-partitioning/src/test/kotlin/com/example/springbatchpartitioning/SpringBatchPartitioningApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.springbatchpartitioning
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBatchPartitioningApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
