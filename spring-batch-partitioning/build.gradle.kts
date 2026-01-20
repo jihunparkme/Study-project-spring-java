a43c6a72e5f0efeb1d43f2195ae080665a570201
Jihun Park <jihunpark.tech@gmail.com>
2026-01-20 21:50:14 +0900

    feat: add dependency

--- spring-batch-partitioning/build.gradle.kts
+++ spring-batch-partitioning/build.gradle.kts
@@ -23,8 +23,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-batch")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
+    runtimeOnly("com.h2database:h2")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-batch-test")
+    testImplementation("org.springframework.batch:spring-batch-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
