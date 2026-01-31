a9f1270bfe23642a0dbc18d2ac84057e5d5cd73a
Jihun Park <jihunpark.tech@gmail.com>
2026-01-31 22:26:53 +0900

    feat: rename parameter

--- spring-batch-partitioning/src/main/kotlin/com/example/springbatchpartitioning/compare/RangePartitioner.kt
+++ spring-batch-partitioning/src/main/kotlin/com/example/springbatchpartitioning/compare/RangePartitioner.kt
@@ -5,16 +5,16 @@ import org.springframework.batch.item.ExecutionContext
 import kotlin.math.min
 
 class RangePartitioner(
-    private val totalAmount: Int,
+    private val totalCount: Int,
     private val stepSize: Int
 ) : Partitioner {
 
     override fun partition(gridSize: Int): Map<String, ExecutionContext> {
         // 0부터 totalAmount-1까지 stepSize만큼 건너뛰며 범위를 생성
-        return (0 until totalAmount step stepSize)
+        return (0 until totalCount step stepSize)
             // 파티션 번호(index)와 시작값(start
             .mapIndexed { index, start ->
-                val end = min(start + stepSize - 1, totalAmount - 1)
+                val end = min(start + stepSize - 1, totalCount - 1)
 
                 "partition$index" to ExecutionContext().apply {
                     putInt("fromIndex", start)
