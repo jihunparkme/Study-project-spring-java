c514fe1f82d7380430c14e034b00500601ff6627
Jihun Park <jihunpark.tech@gmail.com>
2026-02-01 15:09:04 +0900

    refactor: comment

--- spring-batch-partitioning/src/main/kotlin/com/example/springbatchpartitioning/compare/RangePartitioner.kt
+++ spring-batch-partitioning/src/main/kotlin/com/example/springbatchpartitioning/compare/RangePartitioner.kt
@@ -8,11 +8,10 @@ class RangePartitioner(
     private val totalCount: Int,
     private val stepSize: Int
 ) : Partitioner {
-
     override fun partition(gridSize: Int): Map<String, ExecutionContext> {
-        // 0부터 totalAmount-1까지 stepSize만큼 건너뛰며 범위를 생성
+        // 1부터 totalAmount까지 stepSize만큼 건너뛰며 범위를 생성
         return (1 until totalCount + 1 step stepSize)
-            // 파티션 번호(index)와 시작값(start
+            // index: 파티션 번호, start: 시작값
             .mapIndexed { index, start ->
                 val end = min(start + stepSize - 1, totalCount)
 
