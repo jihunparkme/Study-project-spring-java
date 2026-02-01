e62e353df48a5fbdf4ef994324d5fd56430a00bd
Jihun Park <jihunpark.tech@gmail.com>
2026-02-01 14:47:21 +0900

    feat: change range partitioner

--- spring-batch-partitioning/src/main/kotlin/com/example/springbatchpartitioning/compare/RangePartitioner.kt
+++ spring-batch-partitioning/src/main/kotlin/com/example/springbatchpartitioning/compare/RangePartitioner.kt
@@ -11,10 +11,10 @@ class RangePartitioner(
 
     override fun partition(gridSize: Int): Map<String, ExecutionContext> {
         // 0부터 totalAmount-1까지 stepSize만큼 건너뛰며 범위를 생성
-        return (0 until totalCount step stepSize)
+        return (1 until totalCount + 1 step stepSize)
             // 파티션 번호(index)와 시작값(start
             .mapIndexed { index, start ->
-                val end = min(start + stepSize - 1, totalCount - 1)
+                val end = min(start + stepSize - 1, totalCount)
 
                 "partition$index" to ExecutionContext().apply {
                     putInt("fromIndex", start)
